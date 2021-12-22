7387752266137f3adf537333c8bcabcb49bcfae0
edublkc <89402489+edublkc@users.noreply.github.com>
2021-12-22 11:40:16 -0300

    Created upload images

--- public/assets/js/profile.js
+++ public/assets/js/profile.js
@@ -1,10 +1,12 @@
-import {header, closeModal} from "./components.js"
+import { header, closeModal } from "./components.js"
 
 header()
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import * as firebase from "https://www.gstatic.com/firebasejs/9.6.1/firebase-storage.js";
+
 import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
-import { getFirestore, collection, getDocs, addDoc, doc, updateDoc, serverTimestamp,query,orderBy,where } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+import { getFirestore, collection, getDocs, addDoc, doc, updateDoc, serverTimestamp, query, orderBy, where } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyBA3z6tyOTfwlExomg10Fq9eX95-8TQw10",
@@ -19,8 +21,16 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const db = getFirestore(app);
+const storage = firebase.getStorage()
+
+
+
+
+//var profileImages = storageRef.child('ProfilesImages')
+
 const allPostsCollection = collection(db, 'posts')
-const allProfileCollection = collection(db,'profiles')
+const allProfileCollection = collection(db, 'profiles')
+
 
 var currentUserProfile = {}
 
@@ -29,7 +39,7 @@ function getUser() {
     onAuthStateChanged(auth, (user) => {
         if (user) {
             const userProfileName = document.querySelector('#menu-profile--name')
-           readProfileInformations(user.uid)
+            readProfileInformations(user.uid)
             userProfileName.innerHTML = user.email
 
         } else {
@@ -39,23 +49,24 @@ function getUser() {
 }
 
 
-async function readProfileInformations (currentUserUid){
-    
+async function readProfileInformations(currentUserUid) {
+
     const profile = await getDocs(allProfileCollection)
-    profile.docs.forEach((profile)=>{
-        if(profile.id == currentUserUid){
+    profile.docs.forEach((profile) => {
+        if (profile.id == currentUserUid) {
             currentUserProfile = profile.data()
+            currentUserProfile.id = profile.id
         }
-        
+
     })
 
     renderProfileInfo(currentUserProfile)
 }
 
 
-function renderProfileInfo(currentUserProfile){
+function renderProfileInfo(currentUserProfile) {
     console.log(currentUserProfile)
-    const fullName = currentUserProfile.name +' '+currentUserProfile.lastName
+    const fullName = currentUserProfile.name + ' ' + currentUserProfile.lastName
     const location = `${currentUserProfile.city},${currentUserProfile.state} - ${currentUserProfile.country}`
 
     const profileName = document.querySelector('.profile-header--infos__about--name')
@@ -70,7 +81,7 @@ function renderProfileInfo(currentUserProfile){
     profileLocation.innerHTML += location
 }
 
-window.onload = () =>{
+window.onload = () => {
     getUser()
 }
 
@@ -89,8 +100,59 @@ const editProfileButton = document.querySelector('.profile-edit')
 const modal = document.querySelector('.modal')
 const body = document.querySelector('body')
 
-editProfileButton.addEventListener('click',()=>{
+editProfileButton.addEventListener('click', () => {
     modal.classList.add('openned-big')
     body.style.overflow = "hidden"
     closeModal()
 })
+
+
+const saveChangesButton = document.querySelector('#profile-save-changes')
+
+saveChangesButton.addEventListener('click', updateProfileInformations)
+
+
+async function updateProfileInformations() {
+    const updateInfosObj = {}
+
+    //Upload images
+    const allUploadInputs = document.querySelectorAll('[data-image]')
+
+
+
+    for (let i = 0; i < allUploadInputs.length; i++) {
+       
+        let uploadInputField = allUploadInputs[i].getAttribute('id').split('-')
+
+
+        if (allUploadInputs[i].files.length > 0) {
+            const storageRef = firebase.ref(storage, `ProfilesImages/${currentUserProfile.id}_${uploadInputField[1]}`)
+            const upload = await firebase.uploadBytesResumable(storageRef, allUploadInputs[i].files[0])
+            const url = await firebase.getDownloadURL(storageRef).then((urlImage) => updateInfosObj[uploadInputField[1]] = urlImage)
+        }
+
+    }
+
+
+    //Upload personal informations
+    const allInputs = document.querySelectorAll('[data-profile]')
+
+
+    for (let i = 0; i < allInputs.length; i++) {
+        let idInput = allInputs[i].getAttribute('id')
+        let updateName = allInputs[i].getAttribute('id').split('-')
+        let documentValue = document.querySelector(`#${idInput}`).value.trim()
+
+        if (documentValue) {
+            updateInfosObj[updateName[1]] = documentValue
+        }
+
+    }
+
+    const userProfileRef = doc(db, 'profiles', currentUserProfile.id)
+    const profileUpdate = await updateDoc(userProfileRef, updateInfosObj)
+
+
+    window.location.reload()
+}
+
