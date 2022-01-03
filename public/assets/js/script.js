3c4b0c7ff3a4747b2e8722b5f682de8b7d9ddabf
edublkc <89402489+edublkc@users.noreply.github.com>
2022-01-03 14:30:56 -0300

    Bug system like fixed

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,6 +1,6 @@
 //SCRIPT
 
-import { renderPosts } from "./posts.js";
+//import { renderPosts } from "./posts.js";
 
 import { header, newPost } from "./components.js"
 
@@ -178,3 +178,150 @@ logoutButton.addEventListener('click', () => {
             window.location.replace('login.html')
         })
 })
+
+
+//TESTE POST
+
+function renderPosts(posts, currentUserProfile) {
+    const feedPostArea = document.querySelector('#feed-area')
+
+   // console.log(posts)
+
+    feedPostArea.innerHTML = ''
+
+    for (let i = 0; i < posts.length; i++) {
+        feedPostArea.innerHTML += `
+        <div class="feed-area">
+        <div class="feed-area--header">
+            <div class="feed-area--header__left-side">
+                <div class="feed-area--pic">
+                    <img src="${posts[i].post.pic}">
+                </div>
+                <div class="feed-area--infos">
+                    <a href="/public/profile.html?id=${posts[i].post.ownerId}"><span class="feed-area--infos__name">${posts[i].post.owner}</span></a>
+                    <span class="feed-area--infos__date">${posts[i].post.date.toDate()}</span>
+                </div>
+            </div>
+    
+            <div class="feed-area--header__right-side">
+                <i class="fas fa-ellipsis-v feed-area--more__button"></i>
+            </div>
+        </div>
+    
+        <div class="feed-area--post">
+            <p>${posts[i].post.text}</p>
+        </div>
+    
+        <div class="line"></div>
+            <div class="feed-area--interactions">
+                <div class="feed-area--stars__button" data-like="id-${i}">
+                    <span data-ok="id-${i}"><i class="far fa-star"></i></span>
+                    <span data-num="id-${i}">${posts[i].post.like.length}</span>
+                </div>
+                <div class="feed-area--comments__button">
+                    <span><i class="far fa-comment-alt"></i></span>
+                    <span>0</span>
+                </div>
+            </div>
+        <div class="line"></div>
+    
+        <div class="feed-area--all-comments">
+
+            <div class="feed-area--comment">
+                <div class="feed-area--comment__pic">
+                    <img src="./assets/images/avatar.jpg">
+                </div>
+                <div class="feed-area--comment__wrap">
+                    <div class="feed-area--comment__user">
+                        <a href=""><span>Eduardo Mota</span></a>
+                    </div>
+                    <div class="feed-area--comment__text">
+                        <p>${posts[i].post.comments}</p>
+                    </div>
+                </div>
+            </div>     
+           
+        </div>
+    
+        <div class="feed-area--new-comment">
+            <div class="feed-area--new-comment__pic">
+                <img src="${currentUserProfile.pic}">
+            </div>
+            <div class="feed-area--new-coment__input">
+                <input type="text" placeholder="Escreva um comentário">
+            </div>
+        </div>
+        `
+
+        setTimeout(() => {
+            
+            let likeButton = document.querySelector(`[data-like="id-${i}"]`)
+           
+
+            likeButton.addEventListener('click', function () {
+
+                systemLike(posts[i].id, posts[i].post.like, currentUserProfile.id, i)
+            })
+
+            
+        }, 200)
+
+        
+    }
+
+    renderLikes(posts,currentUserProfile.id)
+}
+
+
+async function renderLikes(posts,uid){
+    console.log(posts)
+
+    for(let i = 0; i < posts.length; i++){
+        let likeStar = document.querySelector(`[data-ok="id-${i}"]`)
+        let likeNum = document.querySelector(`[data-num="id-${i}"]`)
+
+        for(let j = 0; j < posts[i].post.like.length; j++){
+            if(posts[i].post.like[j] == uid){
+                    likeStar.classList.add('liked')
+                    likeStar.innerHTML = `<i class="fas fa-star"></i>`
+                    likeNum.classList.add('liked')
+            }
+        }
+    }
+}
+
+async function systemLike(postId, amountLike, uid, id) {
+    let likeStar = document.querySelector(`[data-ok="id-${id}"]`)
+    let likeNum = document.querySelector(`[data-num="id-${id}"]`)
+
+
+    let isLiking = amountLike.filter(likeId => likeId == uid)
+
+    //removing like
+    if(isLiking.length > 0){
+        let likesWithOutMyLike = amountLike.filter(likeId => likeId != uid)
+
+        const postRef = doc(db, 'posts', postId)
+        const profileUpdate = await updateDoc(postRef, { like: likesWithOutMyLike })
+
+        likeStar.classList.remove('liked')
+        likeStar.innerHTML = `<i class="far fa-star"></i>`
+        likeNum.classList.remove('liked')
+    }
+    //adding like
+    else{
+        let likesWithMyLike = amountLike.filter(likeId => likeId != uid)
+
+        const postRef = doc(db, 'posts', postId)
+        const profileUpdate = await updateDoc(postRef, { like: [...likesWithMyLike, uid] })
+
+        likeStar.classList.add('liked')
+        likeStar.innerHTML = `<i class="fas fa-star"></i>`
+        likeNum.classList.add('liked')
+    }
+
+    
+        readProfileInformations(uid)
+    
+    
+}
